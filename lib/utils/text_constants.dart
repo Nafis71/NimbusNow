5b42081177f0dbf528fdff3d02df30e0e5279798
Nafis71 <nhtonmoy2@gmail.com>
2024-06-22 19:38:21 +0600

    api key updated

--- lib/utils/text_constants.dart
+++ lib/utils/text_constants.dart
@@ -1,3 +1,3 @@
 const String currentWeatherApiBaseUrl = "https://api.weatherapi.com/v1/current.json?";
 const String forecastApiBaseUrl = "https://api.weatherapi.com/v1/forecast.json?";
-const String apiKey = "4d0cd00f439c4773af975152242305";
+const String apiKey = "0c0785b38bd447a6be4120906242206";
